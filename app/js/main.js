7cd3c56f9a1d3e34e27ce6c7656d18c89e65db53
k1rk4 <hinhurin@ya.ru>
2021-09-24 11:18:23 +0300

    added mixitup to index page

--- app/js/main.js
+++ app/js/main.js
@@ -1,6 +1,18 @@
 $(function() {
 
-   $('.product-list__filter-btn').on('click', function () {
+  var containerEl1 = document.querySelector('[data-ref="container-1"]');
+  var containerEl2 = document.querySelector('[data-ref="container-2"]');
+ 
+  var config = {
+    controls: {
+      scope: 'local'
+    }
+  };
+ 
+  var mixer1 = mixitup(containerEl1, config);
+  var mixer2 = mixitup(containerEl2, config);
+
+  $('.product-list__filter-btn').on('click', function () {
     $('.product-list__filters').slideToggle();
   })
 
